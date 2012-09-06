44f39401b8e641c66df5aa9847be438b27e6c45c
Rikard Lundmark <rikard.lundmark@yahoo.se>
2012-09-06 15:23:00 +0800

    Continuing setting up repro.

--- setup.sh
+++ setup.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
-found="not";
 
 if [ "$1" == 'twocol' ]; then
     cp -rn templates/twocolumnreport/* ..
-    found="yes";
+    cp -rn templates/global_onetwo/* ..
+    exit 0;
 fi
 
 if [ "$1" == 'onecol' ]; then
     cp -rn templates/onecolumnreport/* ..
-    found="yes";
+    cp -rn templates/global_onetwo/* ..
+    exit 0;
 fi
 
 if [ "$1" == 'lecture' ]; then
     cp -rn templates/lecture/* ..
-    found="yes";
-fi
-
-if [ "$found" == 'yes' ]; then
-    cp -rn templates/global/* ..
     exit 0;
 fi
-
 
 echo "Use to set up a report in parent directory."
 echo "Currently supported arguments:"
 echo "twocol"
+echo "onecol"
+echo "lecture"
 exit 1;
