5ecfa9f29377a524331491c8c8c2a16c09c1cf2f
Rikard Lundmark <rikard.lundmark@yahoo.se>
2012-09-06 15:17:33 +0800

    Chaning setup.

--- setup.sh
+++ setup.sh
@@ -2,25 +2,27 @@
 found="not";
 
 if [ "$1" == 'twocol' ]; then
-    cp -n templates/twocolumnreport/* ..
+    cp -rn templates/twocolumnreport/* ..
     found="yes";
 fi
 
 if [ "$1" == 'onecol' ]; then
-    cp -n templates/onecolumnreport/* ..
+    cp -rn templates/onecolumnreport/* ..
     found="yes";
 fi
 
 if [ "$1" == 'lecture' ]; then
-    cp -n templates/lecture/* ..
+    cp -rn templates/lecture/* ..
     found="yes";
 fi
 
 if [ "$found" == 'yes' ]; then
-    cp -n templates/global/* ..
+    cp -rn templates/global/* ..
+    exit 0;
 fi
 
 
 echo "Use to set up a report in parent directory."
 echo "Currently supported arguments:"
 echo "twocol"
+exit 1;
